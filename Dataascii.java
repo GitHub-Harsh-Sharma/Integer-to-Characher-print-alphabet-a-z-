e55fcacdf36f3ed058a0883ac6a3fea4cc37fb72
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2023-03-04 08:57:19 +0530

    Add files via upload

---
+++ Dataascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Dataasciifor3
+{
+    public static void main(String args[])
+    {
+        int i;
+        Scanner sc = new Scanner(System.in);
+        for(i=65;i<=90;i++)
+        {
+            System.out.print((char)i);
+        }
+    }
+}    
